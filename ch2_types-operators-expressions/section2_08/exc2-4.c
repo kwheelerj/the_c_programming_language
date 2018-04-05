7e4ad8fd18d1efb037e046e433c8f6b6866cc79d
Kenneth Wheeler <kwheelerj@gmail.com>
2018-04-05 13:31:30 -0400

    Ch2 section 8 completed exercises

---
+++ ch2_types-operators-expressions/section2_08/exc2-4.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+/* Write an alternate version of "squeeze(s1, s2)" that deletes each
+ * character in "s1" that matches any character in string "s2".
+ */
+
+#define MAXLIMIT	1000
+
+int mygetline(char s[], int limit);
+void squeeze(char s1[], char s2[]);
+
+int main()
+{
+	char line1[MAXLIMIT];
+	char line2[MAXLIMIT];
+	
+	mygetline(line1, MAXLIMIT);
+	mygetline(line2, MAXLIMIT);
+
+	squeeze(line1, line2);
+	printf("\n%s\n", line1);
+
+	return 0;
+}
+
+int mygetline(char s[], int limit)
+{
+	int i;
+	char c;
+
+	for (i=0; i < limit-1 && (c = getchar()) != EOF && c != '\n'; i++)
+		s[i] = c;
+	if (c == '\n')
+		s[i++] = c;
+	s[i] = '\0';
+
+	return i;
+}
+
+void squeeze(char s1[], char s2[])
+{
+	int i, j, k, flag;
+	j = 0;	
+	
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		flag = 0;
+
+		for (k = 0; s2[k] != '\0'; k++)
+		{
+			if (s1[i] == s2[k])
+				flag = 1;
+		}
+		
+		if (!flag)
+			s1[j++] = s1[i];
+	}
+	s1[j] = '\0';
+}
---
+++ ch2_types-operators-expressions/section2_08/exc2-5.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+/* Write the function "any(s1, s2)", which returns the first location in
+ * the string "s1" where any character from the string "s2" occurs,
+ * or "-1" if "s1" contains no characters from "s2".  (The standard library
+ * function "strpbrk" does the same job but returns a pointer to the location.)
+ */
+
+#define MAXLIMIT	1000
+
+int mygetline(char s[], int limit);
+int any(char s1[], char s2[]);
+
+int main()
+{
+	char line1[MAXLIMIT];
+	char line2[MAXLIMIT];
+	int location;
+
+	mygetline(line1, MAXLIMIT);
+	mygetline(line2, MAXLIMIT);
+
+	location = any(line1, line2);
+	printf("Location found: %d\n", location);
+
+	return 0;
+}
+
+int mygetline(char s[], int limit)
+{
+	int i;
+	char c;
+
+	for (i=0; i < limit-1 && (c = getchar()) != EOF && c != '\n'; i++)
+		s[i] = c;
+	if (c == '\n')
+		s[i++] = '\0';
+	//s[i] = '\0';
+
+	return i;
+}
+
+int any(char s1[], char s2[])
+{
+	int i, k, loc;
+	loc = -1;
+	
+	for (i = 0; s1[i] != '\0' && loc == -1; i++)
+	{
+		for (k = 0; s2[k] != '\0'; k++)
+		{
+			if (s1[i] == s2[k])
+			{
+				loc = i;
+				break;
+			}
+				 
+		}
+		
+	}
+	return loc;
+}
