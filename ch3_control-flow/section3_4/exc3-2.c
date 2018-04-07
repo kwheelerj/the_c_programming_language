140bac24f35bf9e67fe5d7e10853c98ae5dc2baa
Kenneth Wheeler <kwheelerj@gmail.com>
2018-04-07 13:40:29 -0400

    Ch3 section 4, exc 3-2 part 1 complete

---
+++ ch3_control-flow/section3_4/exc3-2.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+
+#define MAXLIMIT	10000
+/* Write a function "escape(s, t)" that converts characters like newline
+ * and tab into visible sequences like "\n" and "\t" as it copies the string
+ * t to s.  Use a switch.  Write a function for the other direction as well,
+ * converting escape sequences into the real characters.
+ */
+
+int mygetline(char s[], int limit);
+void escape(char t[], char s[], int limit);
+
+int main()
+{
+	char line[MAXLIMIT];
+	char escaped_line[MAXLIMIT];
+
+	mygetline(line, MAXLIMIT);
+	escape(line, escaped_line, MAXLIMIT);
+	
+	printf("%s\n", escaped_line);
+
+	return 0;
+}
+
+void escape(char t[], char s[], int limit)
+{
+	int i, j = 0;
+
+	for (i = 0; i < limit-1 && j < limit-1 && t[i] != '\0'; i++)
+	{
+		switch (t[i])
+		{
+		case '\n':
+			if (j < limit-2)
+			{	
+				s[j++] = '\\';
+				s[j++] = 'n';
+			}
+			else
+				s[j++] = '*';
+			break;
+		case '\t':
+			if (j < limit-2)
+			{
+				s[j++] = '\\';
+				s[j++] = 't';
+			}
+			else
+				s[j++] = '*';
+			break;
+		default:
+			s[j++] = t[i];
+			break;
+		}
+	}
+	s[j] = '\0';
+
+}
+
+int mygetline(char s[], int limit)
+{
+	int i;
+	char c;
+
+	for (i=0; i<limit-1 && (c = getchar()) != EOF && c != '\n'; i++)
+		s[i] = c;
+	if (c= '\n')
+		s[i++] = c;
+	s[i] = '\0';
+
+	return i;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
