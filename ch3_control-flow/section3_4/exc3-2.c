3f3de2f2b81b090b0458f3269af9987eead90ddb
Kenneth Wheeler <kwheelerj@gmail.com>
2018-04-07 13:56:32 -0400

    Ch3 section 4 exc3-2 part b complete, sect 4 done

--- ch3_control-flow/section3_4/exc3-2.c
+++ ch3_control-flow/section3_4/exc3-2.c
@@ -9,19 +9,54 @@
 
 int mygetline(char s[], int limit);
 void escape(char t[], char s[], int limit);
+void unescape(char t[], char s[], int limit);
 
 int main()
 {
 	char line[MAXLIMIT];
 	char escaped_line[MAXLIMIT];
+	char unescaped_line[MAXLIMIT];
 
 	mygetline(line, MAXLIMIT);
 	escape(line, escaped_line, MAXLIMIT);
-	
+	unescape(escaped_line, unescaped_line, MAXLIMIT);
+
 	printf("%s\n", escaped_line);
+	printf("%s\n", unescaped_line);
 
 	return 0;
 }
+
+
+void unescape(char t[], char s[], int limit)
+{
+	int i, j = 0;
+
+	for (i = 0; i < limit-1 && t[i] != '\0'; i++)
+	{
+		if (t[i] == '\\')
+		{
+			switch(t[i+1])
+			{
+				case 't':
+					s[j++] = '\t';				
+					i++;
+					break;
+				case 'n':
+					s[j++] = '\n';
+					i++;
+					break;
+				default:
+					s[j++] = t[i];
+			}
+
+		}
+		else
+			s[j++] = t[i];
+	}
+	s[j] = '\0';
+}
+
 
 void escape(char t[], char s[], int limit)
 {
